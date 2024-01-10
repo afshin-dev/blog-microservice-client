5a40236fee1fe9d209e628a20184fb8dc9ec573d
afshin <shahinof10009@gmail.com>
2024-01-11 03:01:23 +0330

    add Posts component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import CssBaseline from "@mui/material/CssBaseline";
+import { Outlet } from "react-router-dom";
 function App() {
   console.log(CssBaseline);
 
   return (
     <>
       <CssBaseline />
-      blog microservice application
+      <nav>nav bar</nav>
+      <Outlet></Outlet>
     </>
   );
 }
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import Posts from "../components/Posts";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
-    index: true,
     errorElement: <div>Page does not exist</div>,
+    children: [
+      {
+        path: "posts",
+        element: <Posts></Posts>,
+      },
+    ],
   },
 ]);

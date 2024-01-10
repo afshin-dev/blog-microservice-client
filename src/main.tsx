38e81a9ed4ec10629bb612a387277141067642a2
afshin <shahinof10009@gmail.com>
2024-01-11 02:26:33 +0330

    provide router

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/index";
 // material font
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -10,6 +10,6 @@ import "@fontsource/roboto/700.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );

bd92ecc7cfd20feced991fcb365485ef1ed8a701
afshin <shahinof10009@gmail.com>
2024-01-12 02:03:12 +0330

    a single post markup

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,13 +1,20 @@
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { singlePost } from "../store/selectors/posts.selector";
-import { Container } from "@mui/material";
+import { Container, Divider, Typography } from "@mui/material";
 
 const Post = () => {
   const { postId } = useParams();
   const post = useSelector(singlePost(postId!));
 
-  return <Container>{post?.title}</Container>;
+  return (
+    <Container sx={{ mt: 3 }}>
+      <Typography variant="h6">{post?.title}</Typography>
+      <Divider></Divider>
+      <Divider></Divider>
+      <Typography sx={{ mt: 3 }}>{post?.content}</Typography>
+    </Container>
+  );
 };
 
 export default Post;
